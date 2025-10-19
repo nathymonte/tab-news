3a25ae0d83763f435e79e44718328a58ca4562d0
Nathalia Monteiro <n.monteiro@unifesp.br>
2025-10-19 16:51:47 -0300

    fixing databse close connection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,7 +3,7 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const databaseName = "local_db";
+  const databaseName = process.env.POSTGRES_DB;
   const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
@@ -14,9 +14,10 @@ async function status(request, response) {
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections;
 
-  const databaseOpenedConnectionsResult = await database.query(
-    `SELECT count(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}'`,
-  );
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
 
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
